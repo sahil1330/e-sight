2d4342873e20b1fdb31bc28d58ab86545eecbcd8
Sahil Mane <eshkon.sahil@gmail.com>
2025-09-12 13:12:08 +0530

    fix: Correct device model name from "e-Sight pro" to "e-Kaathi pro"

--- utils/devices.ts
+++ utils/devices.ts
@@ -1,7 +1,7 @@
 
 export const registeredDevices = [
     {
-        "model": "e-Sight pro",
+        "model": "e-Kaathi pro",
         "serviceUUID": "28ff2720-f39d-437e-8844-c0d161e33c28",
         "characteristicUUID": "2c95435f-15e4-41c0-8b57-300d1c095d1c"
     }
